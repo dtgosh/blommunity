51040a9249b826b0ba64efc78d0cc3bf88dd6dc6
dtgosh <dtg8@naver.com>
2026-02-22 15:19:24 +0900

    refactor: simplify CliModule imports by removing unnecessary config options

--- apps/cli/src/cli.module.ts
+++ apps/cli/src/cli.module.ts
@@ -1,12 +1,11 @@
-import { configModuleOptions } from '@app/config';
+import { ConfigModule } from '@app/config';
 import { DbModule } from '@app/db';
 import { UtilModule } from '@app/util';
 import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { BasicCommand } from './commands/basic.command';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOptions), DbModule, UtilModule],
+  imports: [ConfigModule, DbModule, UtilModule],
   providers: [Logger, BasicCommand],
 })
 export class CliModule {}
