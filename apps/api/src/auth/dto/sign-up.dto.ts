649a9a20140753c3e5d19674cfe1a973c15fb1ee
dtgosh <dtg8@naver.com>
2026-03-03 00:29:12 +0900

    chore: 상속하여 중복 제거

--- apps/api/src/auth/dto/sign-up.dto.ts
+++ apps/api/src/auth/dto/sign-up.dto.ts
@@ -1,27 +1,12 @@
 import { ApiSchema } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsOptional } from 'class-validator';
+import { SignInDto } from './sign-in.dto';
 
 @ApiSchema({
   name: '회원가입 계정 정보',
   description: '회원가입 요청 시 전달하는 계정 정보',
 })
-export class SignUpDto {
-  /** 사용자 아이디 */
-  @IsNotEmpty()
-  @IsString()
-  public username!: string;
-
-  /** 비밀번호 (8자 이상) */
-  @IsNotEmpty()
-  @MinLength(8)
-  public password!: string;
-
+export class SignUpDto extends SignInDto {
   /** 이메일 주소 */
   @IsOptional()
   @IsEmail()
